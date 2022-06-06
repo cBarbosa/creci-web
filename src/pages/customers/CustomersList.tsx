2c5fb4089dbf226334e6b69ba989111f4915277c
Charles Barbosa <xbrown@gmail.com>
2022-06-06 18:53:56 -0300

    fix call list customers

--- src/pages/customers/CustomersList.tsx
+++ src/pages/customers/CustomersList.tsx
@@ -13,6 +13,7 @@ type CustomerType = {
     uuid: string;
     name: string;
     email: string;
+    phone: string;
     addresses: AddressType[];
 };
 
@@ -29,14 +30,18 @@ const CustomersListPage: React.FunctionComponent<ICustomersListPageProps> = (pro
     const [customers, setCustomers] = useState<CustomerType[]>([]);
 
     useEffect(() => {
-       const result:any = api.get('/api/Customer/paging')
+       api.get('/api/Customer/paging').then(result => {
+            // console.debug('data items',result?.data?.data?.items);
+            // console.debug('totalCount', result?.data?.data?.totalCount);
+            if(result?.data?.success) {
+                console.log('passo 1')
+                setCustomers(result?.data?.data?.items);
+            }
+       })
         .catch((error) => {
          console.log(error);
       });
-
-       if(result?.data?.data?.sucess)
-         setCustomers(result?.data?.data?.items);
-       console.debug('data items',result?.data);
+       
   }, []);
 
     return (
@@ -45,18 +50,16 @@ const CustomersListPage: React.FunctionComponent<ICustomersListPageProps> = (pro
 
             {customers.map((customer, index) => {
                 return (
-                <div key={index} className="p-4 mb-4 rounded-lg flex items-center space-x-4 bg-slate-200">
-                    <img className="w-10 h-10 rounded-full ring-2 ring-gray-300" src="https://images.unsplash.com/photo-1520315342629-6ea920342047?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGNhdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
-                    <div className="space-y-1 font-medium dark:text-gray-500">
-                        <div>{customer.name}</div>
-                        <div className="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
+                    <div key={index} className="p-4 mb-4 rounded-lg flex items-center space-x-4 bg-slate-200">
+                        <img className="w-10 h-10 rounded-full ring-2 ring-gray-300" src="https://images.unsplash.com/photo-1520315342629-6ea920342047?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGNhdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
+                        <div className="space-y-1 font-medium dark:text-gray-500">
+                            <div>{customer.name}</div>
+                            <div className="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
+                        </div>
                     </div>
-                </div>
                 )
             })}
                 
-            
-
             
         </>
         );
